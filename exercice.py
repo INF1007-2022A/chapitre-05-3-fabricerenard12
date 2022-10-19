71e70b672a32ead29d83b13ce28486da50dd3711
fabricerenard12 <fabrice.renard12@outlook.com>
2022-10-19 10:12:37 -0400

    Exercices complétés

--- exercice.py
+++ exercice.py
@@ -4,21 +4,67 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	res = 0
+
+	for letter in text:
+		if letter.isalnum():
+			res += 1
+
+	return res
 
 def get_word_length_histogram(text):
-	return [0]
+	res = ''
+
+	for i in text:
+		if i == ' ':
+			res += i
+		elif i.isalnum():
+			res += i
+
+	max_length = len(max(res.split(), key = lambda x: len(x)))
+	arr = [0] * (max_length + 1)
+
+	for i in res.split():
+		arr[len(i)] += 1
+		
+	return arr
+
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
+	res = ''
 
-	return ""
+	for i in range(1, len(histogram)):
+		if i < 10:
+			res += ' '
+		res += str(i) + ' ' + ROW_CHAR * histogram[i] + '\n'
+	
+	return res
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
 
-	return ""
+	rows = []
+	rows.append(LINE_CHAR * len(histogram))
+	chart = ''
+
+	for i in range(1, max(histogram) + 1):
+		res = ''
+		for j in histogram[1:]:
+			if j >= i:
+				res += BLOCK_CHAR
+			else:
+				res += ' '
+		rows.append(res)
+
+	rows.reverse()
+
+	for i in rows:
+		chart += i + '\n'
+
+	print(chart)
+	return chart
 
 
 if __name__ == "__main__":
